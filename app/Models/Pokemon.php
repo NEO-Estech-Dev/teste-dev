099c1721766cd654f59dd5f8324f244ad01d6a70
Vinicius Zynich <viniciuscardosozynich@gmail.com>
2026-09-01 01:34:03 -0300

    Feat: Cria model e migration

---
+++ app/Models/Pokemon.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pokemon extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'pokeapi_id',
+        'name',
+        'hp',
+        'attack',
+        'defense',
+        'special_attack',
+        'special_defense',
+        'speed',
+        'weight',
+        'height',
+    ];
+}
